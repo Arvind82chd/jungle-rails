7df44c0d4d6bc85c1d081addec5e4814589341c6
Arvind Sharma <mail.arvind.s@gmail.com>
2022-02-07 19:55:51 +0000

    updated the erb files for admin categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -22,11 +22,11 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
-  def destroy
-    @category = Category.find params[:id]
-    @category.destroy
-    redirect_to [:admin, :categories], notice: 'category deleted!'
-  end
+  # def destroy
+  #   @category = Category.find params[:id]
+  #   @category.destroy
+  #   redirect_to [:admin, :categories], notice: 'category deleted!'
+  # end
 
   private
 
